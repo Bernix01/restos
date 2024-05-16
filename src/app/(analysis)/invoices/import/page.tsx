4351d4f9b1cf0bc6522bb3e69442eeeefcb9df36
Bernix01 <gbernal096@gmail.com>
2024-05-16 18:42:43 -0500

    feat: Update credit notes heading to include business category

--- src/app/(analysis)/invoices/import/page.tsx
+++ src/app/(analysis)/invoices/import/page.tsx
@@ -616,7 +616,7 @@ const ImportPage: React.FC = () => {
 
       <h3 className="text-2xl font-bold tracking-tight">Expenses (Business)</h3>
       <ExpensesGraph invoices={businessInvoices} taxesType={taxesTypes} />
-      <h3 className="text-2xl font-bold tracking-tight">Credit notes</h3>
+      <h3 className="text-2xl font-bold tracking-tight">Credit notes (Business)</h3>
       <h4 className="text-1xl font-bold tracking-tight">1st Semester</h4>
       <section className="grid grid-cols-4 gap-2">
         {business1SCreditNotes.map((creditNote, index) => (
